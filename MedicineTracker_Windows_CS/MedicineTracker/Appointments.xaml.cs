2ee71ad4eda2ce0bc30dcf527176d5cb056d0371
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-11-01 18:54:05 +0000

    Added code to allow user add appointments to their calendar

--- MedicineTracker_Windows_CS/MedicineTracker/Appointments.xaml.cs
+++ MedicineTracker_Windows_CS/MedicineTracker/Appointments.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Appointments;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,37 @@ namespace MedicineTracker
         public Appointments()
         {
             this.InitializeComponent();
+        }
+
+        private async void Add_Click(object sender, RoutedEventArgs e)
+        {
+            // Reference link for code below, for adding events - http://stackoverflow.com/questions/38355922/system-argumentexception-in-app-when-accepting-appointment
+
+            var rect = new Rect(new Point(Window.Current.Bounds.Width / 2, Window.Current.Bounds.Height / 2), new Size());
+            DateTimeOffset date = StartDate.Date;
+            TimeSpan time = StartTime.Time;
+            int minutes = int.Parse((string)((ComboBoxItem)Duration.SelectedItem).Tag);
+
+            // Creating a new Event
+            Appointment newEvent = new Appointment()
+            {
+                // Creating a date and time values and getting the current date from OS using TimeZoneInfo.Local.GetUtcOffset(DateTime.Now)
+                Subject = Subject.Text,
+                Location = Location.Text,
+                Details = Details.Text,
+                StartTime = new DateTimeOffset(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0, TimeZoneInfo.Local.GetUtcOffset(DateTime.Now)),
+                Duration = TimeSpan.FromMinutes(minutes)
+            };
+            // The following line triggers the launch of the calendar app for the user to confirm adding the appointment
+            string id = await AppointmentManager.ShowAddAppointmentAsync(newEvent, rect, Placement.Default);
+
+            // Empties all data entered
+            StartDate.Date = DateTime.Now;
+            StartTime.Time = DateTime.Now.TimeOfDay;
+            Subject.Text = string.Empty;
+            Details.Text = string.Empty;
+            Location.Text = string.Empty;
+            Duration.SelectedIndex = 0;
         }
     }
 }
